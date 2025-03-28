669ceddccb33d4e065b7f8be879419c6249a2a28
Crystian Guzman <crystianguzman55@gmail.com>
2025-03-28 10:18:40 -0700

    successfully applied collision detection feature to the player by using the Rectangle class intersects method and checking collision direction player hits, also incrementing or decrementing the player's movement speed when colliding, so their is no overlapping and only collision at that specific position

--- src/Block.java
+++ src/Block.java
@@ -1,8 +1,36 @@
 import java.awt.*;
 
 public class Block extends Rectangle {
-
+    String direction = "right";
+    boolean leftCollision = false;
+    boolean rightCollision = false;
+    boolean upCollision = false;
+    boolean downCollision = false;
     Block(int x, int y, int width, int height) {
         super(x, y, width, height);
+    }
+
+    @Override
+    public boolean intersects(Rectangle block) {
+        int playerWidth = this.width;
+        int playerHeight = this.height;
+        int blockWidth = block.width;
+        int blockHeight = block.height;
+        if (blockWidth <= 0 || blockHeight <= 0 || playerWidth <= 0 || playerHeight <= 0) {
+            return false;
+        }
+        int playerX = this.x;
+        int playerY = this.y;
+        int blockX = block.x;
+        int blockY = block.y;
+        blockWidth += blockX;
+        blockHeight += blockY;
+        playerWidth += playerX;
+        playerHeight += playerY;
+        //      overflow || intersect
+        return ((blockWidth < blockX || blockWidth > playerX) &&
+                (blockHeight < blockY || blockHeight > playerY) &&
+                (playerWidth < playerX || playerWidth > blockX) &&
+                (playerHeight < playerY || playerHeight > blockY));
     }
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -12,21 +12,44 @@ class GamePanel extends JPanel {
         this.screenHeight = screenHeight;
         this.kH = kH;
         setBackground(Color.BLACK);
-        timer = new Timer(50, e -> {
-            if(kH.right && !(player.getX()+player.getWidth() == block.getX())
-                        && !(player.getY()+ player.getHeight() == block.getY())
-            ){
-                player.x+= 10;
-            }
-            else if(kH.left && !( player.getX() == block.getX()+block.getWidth()) ){player.x -= 10;}
-            else if(kH.up) {player.y -= 10;}
-            else if(kH.down){player.y += 10;}
-            repaint();
-        });
-        timer.start();
         setPreferredSize(new Dimension(screenWidth,screenHeight));
         player = new Block(50,this.screenHeight-100,50,50);
         block = new Block(200,this.screenHeight-100,50,50);
+        timer = new Timer(50, e -> {
+            if(kH.left || kH.right || kH.up || kH.down){
+                if(kH.left){player.direction = "left";}
+                if(kH.right){player.direction = "right";}
+                if(kH.up){player.direction = "up";}
+                if(kH.down){player.direction = "down";}
+
+                //System.out.println("rightCollision? " + player.rightCollision);
+                if((!player.leftCollision) && player.direction.equals("left")){player.x -= 10;}
+                if((!player.rightCollision) && player.direction.equals("right")){player.x += 10;}
+                if((!player.upCollision) && player.direction.equals("up")){player.y -= 10;}
+                if((!player.downCollision) && player.direction.equals("down")){player.y += 10;}
+            }
+            player.leftCollision = false;
+            player.rightCollision = false;
+            player.upCollision = false;
+            player.downCollision = false;
+
+            if(player.intersects(block)){
+                switch(player.direction){
+                    case"left" : player.leftCollision = true;player.x += 10;break;
+                    case"right" : player.rightCollision = true;player.x -= 10;break;
+                    case"up" : player.upCollision = true;player.y += 10;break;
+                    case"down" : player.downCollision = true;player.y -= 10;break;
+                }
+            }
+
+
+
+
+
+            repaint();
+        });
+        timer.start();
+
     }
 
     @Override
